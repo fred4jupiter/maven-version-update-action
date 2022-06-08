a0fbab644241b2eed034727a662a9c17b01c1777
Staehler, Michael <michael.staehler@opitz-consulting.com>
2022-06-08 09:20:51 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # maven-version-update-action
+
 Github action for updating the version of a Maven project for release purposes.
---
+++ update-version.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+for i in "$@"
+do
+case $i in
+    -gu=*|--github-username=*)
+    GITHUB_USERNAME="${i#*=}"
+    ;;
+    -gp=*|--github-password=*)
+    GITHUB_PASSWORD="${i#*=}"
+    ;;
+esac
+done
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Usage example: ./update-me.sh -gu=freddy -gp='secret'"
+    exit 1
+fi
+
+if [ -z "$GITHUB_USERNAME" ]
+  then
+    echo "Please define the Github username. Example: -gu=freddy"
+    exit 1
+fi
+
+if [ -z "$GITHUB_PASSWORD" ]
+  then
+    echo "Please define the Github password. Example: -gp=secret"
+    exit 1
+fi
+
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit
+mvn build-helper:parse-version scm:tag -Dbasedir=. -Dtag=release_\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
+PROJECT_REL_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
+NEXT_DEV_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+git commit -a -m "next dev version $NEXT_DEV_VERSION"
+git push
+
+echo "release version is: $PROJECT_REL_VERSION"
+echo "next development version is: $NEXT_DEV_VERSION"
+
+export PROJECT_REL_VERSION
+export NEXT_DEV_VERSION
